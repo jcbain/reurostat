f46df15e056ede6842a58fc252aad7cad96b0d0a
jcbain <jamescbain@gmail.com>
2019-09-25 12:41:13 -0600

    wrap parsing stuff into a temporary function

--- R/functional_urban_areas.R
+++ R/functional_urban_areas.R
@@ -12,15 +12,25 @@ get_fua <- function(dataset, language = "en", city, props = FALSE){
   resp
 }
 
+parse_response <- function(resp){
+  year <- jsonlite::fromJSON(rawToChar(resp$content))$dimension$time$category$label
+  data <- jsonlite::fromJSON(rawToChar(rep$content))$value
+  null_data <- jsonlite::fromJSON(rawToChar(rep$content))$status
+  category <- jsonlite::fromJSON(rawToChar(rep$content))$dimension$indic_ur$category$label
 
-tmp <- jsonlite::fromJSON(rawToChar(rep$content))$dimension$time$category$label
-tmp2 <- jsonlite::fromJSON(rawToChar(rep$content))$dimension$time$category$index
-tmp3 <- jsonlite::fromJSON(rawToChar(rep$content))$value
-tmp4 <- jsonlite::fromJSON(rawToChar(rep$content))$status
+  tibble(
+    year = rep(purrr::map_int(names(year), function(x){as.integer(year[[x]])}),
+               length(c(data, null_data))/length(year)),
+    value = purrr::map_chr(as.character(sort(as.integer(c(names(data),
+                                                          names(null_data))))),
+                           function(x){c(data, null_data)[[x]]}),
+    code = purrr::map(names(category),
+                      function(x){rep(x, 30)}) %>% purrr::reduce(c),
+    dataset = purrr::map(names(category),
+                         function(x){rep(category[[x]], 30)}) %>% purrr::reduce(c)
 
-purrr::map_int(names(tmp2), function(x){as.integer(tmp2[[x]])})
-purrr::map_int(names(tmp3), function(x){as.integer(x)%%length(tmp)})
-purrr::map_int(names(tmp4), function(x){as.integer(x)%%length(tmp)})
-sort(purrr::map_int(c(names(tmp4), names(tmp3)), function(x){as.integer(x)}))
-purrr::map_chr(sort(as.integer(c(names(tmp3), names(tmp4)))), function(x){do.call(c, list(tmp3, tmp4))[[x]]})
-purrr::map_chr(as.character(sort(as.integer(c(names(tmp3), names(tmp4))))), function(x){c(tmp3, tmp4)[[x]]})
+  )
+}
+
+
+
