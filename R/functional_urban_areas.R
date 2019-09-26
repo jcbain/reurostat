1fdcda532e9d6f38505f1db9bd166962e669c388
jcbain <jamescbain@gmail.com>
2019-09-25 23:54:50 -0600

    write documentation to get_fua

--- R/functional_urban_areas.R
+++ R/functional_urban_areas.R
@@ -1,7 +1,33 @@
-#' Functional Urban Areas
+#' Download Functional Urban Areas datasets.
 #'
-get_fua <- function(dataset, language = "en", city, props = FALSE){
-  args <- list(cities = city)
+#' Description from eurostat (see link for reference).
+#' The Functional Urban Area consists of a city and its commuting zone. (This
+#' was formerly known as larger urban zone (LUZ)).
+#'
+#' Download data pertaining to Functional Urban Areas from eurostat. Cities
+#' here refers to the major ubran area in which the Functional Urban Area
+#' belongs to.
+#'
+#' @importFrom magrittr "%>%"
+#'
+#' @param dataset The dataset code pertaining to the variable of interest.
+#' @param cities The functional urban area code or a vector of codes for the
+#'   functional urban areas of interest.
+#' @param language The language of the data.
+#' @param props An option to include proportional data along with absolute
+#'   values.
+#' @return A tibble of with the attributes `fua_code`, `fua`, `year`, `value`,
+#'   `code` and `variable`.
+#' @seealso \url{https://ec.europa.eu/eurostat/web/cities/spatial-units}
+#' @export
+#' @examples \dontrun{
+#' library(reurostat)
+#'
+#' brus_antw <- get_fua(dataset = "urb_lpop1", cities = c("BE002L2", "BE001L2")
+#' }
+get_fua <- function(dataset, cities, language = "en", props = FALSE){
+  args <- purrr::map(cities, function(x){x}) %>%
+    stats::setNames(rep("cities", length(cities)))
 
   check_internet()
   base_url_modified <- sprintf(paste0(base_url, "%s/%s"), language, dataset)
@@ -9,10 +35,6 @@ get_fua <- function(dataset, language = "en", city, props = FALSE){
 
   check_status(resp)
 
-  resp
-}
-
-parse_response <- function(resp){
   rjson <- jsonlite::fromJSON(rawToChar(resp$content))
 
   year <- rjson$dimension$time$category$label
@@ -26,6 +48,13 @@ parse_response <- function(resp){
   num_cities <- rjson$size[2]
   num_years <- rjson$size[3]
 
+  value <- purrr::map_chr(as.character(sort(as.integer(c(names(data),
+                                                         names(null_data))))),
+                          function(x){
+                            c(data, null_data)[[x]]
+                          })
+  value[value == ":"] <- NA
+
   tibble(
     fua_code = rep(purrr::map(names(fua_code), function(x){
       rep(x, num_years)
@@ -37,21 +66,14 @@ parse_response <- function(resp){
       purrr::reduce(c), num_codes),
     year = rep(purrr::map_int(names(year), function(x){as.integer(year[[x]])}),
                num_codes * num_cities),
-    value = purrr::map_chr(as.character(sort(as.integer(c(names(data),
-                                                          names(null_data))))),
-                           function(x){c(data, null_data)[[x]]}),
+    value = as.numeric(value),
     code = purrr::map(names(category),function(x){
-                        rep(x, num_years * num_cities)}
-                      ) %>%
+      rep(x, num_years * num_cities)}
+    ) %>%
       purrr::reduce(c),
     variable = purrr::map(names(category), function(x){
       rep(category[[x]], num_years * num_cities)
-      }) %>%
+    }) %>%
       purrr::reduce(c)
-
   )
-
 }
-
-
-
