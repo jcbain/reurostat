7deded2de0f9a86f6d8947c55cc8bec11e4c59eb
jcbain <jamescbain@gmail.com>
2019-09-25 22:53:07 -0600

    update parsing ability with greater flexibiliy

--- R/functional_urban_areas.R
+++ R/functional_urban_areas.R
@@ -13,23 +13,44 @@ get_fua <- function(dataset, language = "en", city, props = FALSE){
 }
 
 parse_response <- function(resp){
-  year <- jsonlite::fromJSON(rawToChar(resp$content))$dimension$time$category$label
-  data <- jsonlite::fromJSON(rawToChar(rep$content))$value
-  null_data <- jsonlite::fromJSON(rawToChar(rep$content))$status
-  category <- jsonlite::fromJSON(rawToChar(rep$content))$dimension$indic_ur$category$label
+  rjson <- jsonlite::fromJSON(rawToChar(resp$content))
+
+  year <- rjson$dimension$time$category$label
+  data <- rjson$value
+  null_data <- rjson$status
+  category <- rjson$dimension$indic_ur$category$label
+  fua_code <- rjson$dimension$cities$category$label
+  fua <- rjson$dimension$cities$category$label
+
+  num_codes <- rjson$size[1]
+  num_cities <- rjson$size[2]
+  num_years <- rjson$size[3]
 
   tibble(
+    fua_code = rep(purrr::map(names(fua_code), function(x){
+      rep(x, num_years)
+    }) %>%
+      purrr::reduce(c), num_codes),
+    fua = rep(purrr::map(fua, function(x){
+      rep(x, num_years)
+    }) %>%
+      purrr::reduce(c), num_codes),
     year = rep(purrr::map_int(names(year), function(x){as.integer(year[[x]])}),
-               length(c(data, null_data))/length(year)),
+               num_codes * num_cities),
     value = purrr::map_chr(as.character(sort(as.integer(c(names(data),
                                                           names(null_data))))),
                            function(x){c(data, null_data)[[x]]}),
-    code = purrr::map(names(category),
-                      function(x){rep(x, 30)}) %>% purrr::reduce(c),
-    dataset = purrr::map(names(category),
-                         function(x){rep(category[[x]], 30)}) %>% purrr::reduce(c)
+    code = purrr::map(names(category),function(x){
+                        rep(x, num_years * num_cities)}
+                      ) %>%
+      purrr::reduce(c),
+    variable = purrr::map(names(category), function(x){
+      rep(category[[x]], num_years * num_cities)
+      }) %>%
+      purrr::reduce(c)
 
   )
+
 }
 
 
