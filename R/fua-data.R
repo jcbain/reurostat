3144294eaa5e12f30982c6716490138e2966969b
jcbain <jamescbain@gmail.com>
2019-10-04 13:56:37 -0600

    create fua data

---
+++ R/fua-data.R
@@ -0,0 +1,11 @@
+#' Functional Urban Areas (FUA) Data
+#'
+#' Data containing Eurostat functional urban area (fua) names and codes along
+#' with the country each fua belongs to.
+#'
+#' @docType data
+#' @usage data(fua)
+#' @keywords datasets
+#'
+#' @source \href{https://ec.europa.eu/eurostat/web/cities/spatial-units}{Spatial Units}
+"fua"
