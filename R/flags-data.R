af8ed6b0c94579ad81ccaf5893ba73f59ffec8a5
jcbain <jamescbain@gmail.com>
2019-10-04 11:46:54 -0600

    create flags data

---
+++ R/flags-data.R
@@ -0,0 +1,10 @@
+#' Eurostat Flags and Flag Descriptions
+#'
+#' Data pertaining to the flags associated with some of the values in Eurostat.
+#'
+#' @docType data
+#' @usage data(flags)
+#' @keywords datasets
+#'
+#' @source \href{https://ec.europa.eu/eurostat/statistics-explained/index.php?title=Tutorial:Symbols_and_abbreviations#Statistical_symbols.2C_abbreviations_and_units_of_measurement}{Statistical symbols, abbreviations and units of measurement}
+"flags"
