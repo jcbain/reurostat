e38ff5482b576fc4af2ca5ee06c037ee17527d8a
jcbain <jamescbain@gmail.com>
2019-10-04 14:27:07 -0600

    add fua code and city code to city data

---
+++ R/city-data.R
@@ -0,0 +1,11 @@
+#' City Code Data
+#'
+#' Data containing Eurostat functional urban area (fua) and city names and codes
+#' along with the country each fua belongs to.
+#'
+#' @docType data
+#' @usage data(city)
+#' @keywords datasets
+#'
+#' @source \href{https://ec.europa.eu/eurostat/web/cities/spatial-units}{Spatial Units}
+"city"
